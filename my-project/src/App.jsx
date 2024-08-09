3db023f9c776b2248727def15f44f49c2728d806
Eraycan Sivri <eraycansivri@hotmail.com>
2024-08-09 16:50:16 +0300

    lkdlksadnkad

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import User from './Components/User/User';
@@ -7,17 +6,13 @@ import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          {/* "/": Home Component'i gösterir */}
-          <Route path="/" element={<Home />} />                
-          {/* "/users/:userId": User Component'i ve parametreyi gösterir */}
-          <Route path="/users/:userId" element={<User />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users/:userId" element={<User />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx'; // App.jsx dosyasını buraya dahil ediyorsunuz
+import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App /> {/* App bileşenini burada render ediyorsunuz */}
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
